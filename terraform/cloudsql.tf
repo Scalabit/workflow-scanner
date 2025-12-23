9b2be34c696294b2be2916bb13c5a71c8023fbf3
Mariola04 <mariominhava@gmail.com>
2025-12-23 17:36:36 +0000

    added special characters restriction

--- terraform/cloudsql.tf
+++ terraform/cloudsql.tf
@@ -60,6 +60,7 @@ resource "google_sql_user" "workflow_user" {
 resource "random_password" "db_password" {
   length  = 16
   special = true
+  override_special = "!#$%&*()-_=+[]{}<>?"
 }
 
 # Store database password in Secret Manager
