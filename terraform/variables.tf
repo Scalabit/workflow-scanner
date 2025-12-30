b0174580ec52863dd2f51b5e8805b3fb683907f8
rost-andreev <178784808+rost-andreev@users.noreply.github.com>
2025-12-30 10:47:38 +0100

    rename variables (#178)

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,14 +11,14 @@ variable "region" {
 }
 
 # Secrets as input variables
-variable "gh_app_id" {
-  description = "GitHub App ID"
+variable "gh_client_id" {
+  description = "GitHub App Client ID"
   type        = string
   sensitive   = true
 }
 
-variable "gh_app_secret" {
-  description = "GitHub App Secret"
+variable "gh_client_secret" {
+  description = "GitHub App Client Secret"
   type        = string
   sensitive   = true
 }
