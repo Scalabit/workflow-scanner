0f308f84e9a6523ca1b900a6250bc9020bbd3bde
Mariola04 <mariominhava@gmail.com>
2026-03-10 18:09:56 +0000

    more max tokens

--- invoice/main.go
+++ invoice/main.go
@@ -187,7 +187,7 @@ try:
     
     inputs = tokenizer(prompt, return_tensors="pt")
     with torch.no_grad():
-        outputs = model.generate(**inputs, max_length=128, do_sample=False, pad_token_id=tokenizer.eos_token_id)
+        outputs = model.generate(**inputs, max_new_tokens=256, do_sample=False, pad_token_id=tokenizer.eos_token_id)
     
     result = tokenizer.decode(outputs[0], skip_special_tokens=True)
 
